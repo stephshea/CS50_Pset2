ab4c98c9e847d0fa7a98273e3f26b3c39eb4527c
Stephanie Shea <stephcshea@gmail.com>
2018-04-09 21:11:03 +0000

    completed initials

--- Initials/initials.c
+++ Initials/initials.c
@@ -5,18 +5,22 @@
 
 int main()
 {
-    string s = get_string("name: ");
+    string s = get_string("");
     printf("%s\n", s);
-
+    //n = length of s
     int n = strlen(s);
+    //prints string's first initial in uppercase
     printf("%c", (toupper(s[0])));
+    //iterates thru string
     for (int i = 0; i < n; i++)
     {
-
+    //checks for a space between words
         if (s[i] == ' ')
         {
-
+    //if space is found, prints next char in uppercase, then repeats to check for other spaces
             printf("%c", (toupper(s[i + 1])));
         }
     }
+    //prints blank line
+    printf("\n");
 }
