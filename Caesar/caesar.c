343ba985a7412ef6fe1c7fa868c3ac21f507d903
Stephanie Shea <stephcshea@gmail.com>
2018-04-10 20:41:33 +0000

    increasing charcode of string chars

--- Caesar/caesar.c
+++ Caesar/caesar.c
@@ -15,21 +15,30 @@ string k;
 k = get_string("enter cipher integer: ");
 string text = get_string("enter text to cipher: ");
 //get cipher code
-if( argc > 3)
-{
-    printf("too many args");
-    return 0;
-}
-else
-{
-    k = argv[1];
-    int key = atoi(k);
-    printf("%d\n", key);
-}
+// if( argc > 3)
+// {
+//     printf("too many args");
+//     return 0;
+// }
+// else
+// {
+argc = 2;
+ k = argv[1];
+//     int key = atoi(k);
+//     printf("%d\n", key);
+// }
 
-for (int i = 0, n = strlen(text); i < n; i++)
+// for (int i = 0, n = strlen(text); i < n; i++)
+//     {
+//         printf("%c", text[i]);
+//     }
+
+
+//string s = get_string("Name: ");
+    for (int i = 0; i < strlen(text); i++)
     {
-        printf("%c", text[i]);
+        //printing ascii for string + 4 for each char
+        printf("%c %i\n", text[i], (int) text[i]+4);
     }
 }
 
